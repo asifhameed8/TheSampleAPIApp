d4b845bf795b98b06a1d2a6ee68ed2591eb2fafc
Asif Hameed <asif.hameed_8@hotmail.com>
2025-04-22 16:59:30 +0500

    client updated. program.cs cleaned

--- TheSampleAPI/Program.cs
+++ TheSampleAPI/Program.cs
@@ -14,34 +14,14 @@ if (app.Environment.IsDevelopment())
          {
              options.Title = "The Smaple API"; // Set the title for the API reference page
              options.Theme = ScalarTheme.Saturn;
+             options.Layout = ScalarLayout.Modern;
+             options.HideClientButton = true; // Hide the "Try it out" button for client generation
          }
         );    
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
